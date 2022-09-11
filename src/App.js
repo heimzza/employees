7b1759727072d342124bf53313ff5d57a694309e
heimzza <hamzassm18@gmail.com>
2022-09-11 16:01:36 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import {Home} from './components/Home';
+import {Department} from './components/Department';
+import {Employee} from './components/Employee';
 
 function App() {
   return (
     <div className="App">
+      <Home></Home>
+      <Department></Department>
+      <Employee></Employee>
       <h2>Merhaba</h2>
       <Button variant="primary">Tıkla</Button>
     </div>
