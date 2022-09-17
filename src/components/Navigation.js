d2ef9da6e8c9e3e3876851c3118b115e8e4f383c
heimzza <hamzassm18@gmail.com>
2022-09-17 22:38:17 +0300

    minor change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 
 export class Navigation extends Component {
     render() {
