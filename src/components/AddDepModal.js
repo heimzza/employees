e3b0914c88265407830dabcfb40cbb488ea4b288
heimzza <hamzassm18@gmail.com>
2022-09-18 17:37:01 +0300

    add via modal

--- src/components/AddDepModal.js
+++ src/components/AddDepModal.js
@@ -3,6 +3,28 @@ import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
 
 
 export class AddDepModal extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+
+        fetch('http://localhost:57594/api/department', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                DepartmentID: null,
+                DepartmentName: e.target.DepartmentName.value
+            })
+        })
+            .then(res => res.json())
+            .then((result) => {
+                alert(result);
+            },
+                (error) => {
+                    alert(error);
+                })
+    }
     render() {
         return (
             <Modal
@@ -18,7 +40,29 @@ export class AddDepModal extends Component {
                 </Modal.Header>
                 <Modal.Body>
                     <div className='container'>
-                        Add Modal Body
+                        <Row>
+                            <Col sm={6}>
+                                <Form onSubmit={this.handleSubmit}>
+                                    <Form.Group>
+                                        <Form.Label>
+                                            Department Name
+                                        </Form.Label>
+                                        <Form.Control
+                                            type='text'
+                                            name='DepartmentName'
+                                            required
+                                            placeholder='DepartmentName'
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className='mt-3'>
+                                        <Button variant='primary' type='submit'>
+                                            Add
+                                        </Button>
+                                    </Form.Group>
+
+                                </Form>
+                            </Col>
+                        </Row>
                     </div>
                 </Modal.Body>
                 <Modal.Footer>
